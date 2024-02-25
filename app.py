8f5a274252e4f5a47ecb81b214c818c502289cab
Rajtilak Bhattacharjee <rajtilak.blog@gmail.com>
2024-02-25 23:04:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ with st.sidebar:
     st.header("What's this?!", divider='rainbow')
     st.write("This is a simple app that accepts an image from the user and detects if any Dinosaus are present in that image.")
     st.header("But why!", divider='rainbow')
-    st.write("For fun! ... err, not really. I am a Machine Learning Engineer with around 20 years of work experience. I have created a number of apps but I am too afraid to release them. It's because I have been searching for a job but not finding any, which made me think do I really know coding at all. So I thought about starting simple, and that's how this was created.")
+    st.write("For fun! ... err, not really. I am a Machine Learning Engineer with around 20 years of work experience. I have created a number of apps but I am too afraid to release them. It's because I have been searching for a job but not finding any, which made me think do I really know coding at all. So I thought about starting simple, and that's how this was created. <a href="https://www.reddit.com/r/AskReddit/comments/bg8vc/comment/c0mmeos/">Here's where I go the inspiration from.</a>")
     st.header("Noice! What can I do?", divider='rainbow')
     st.write("Simple. Let me know what do you think about this on the GitHub repository for this app; whether you like it, had fun with it, anything. It would work as a moral booster for me. Feel free to contribute if you want. If you are feeling generous, consider donating so that I can continue to create more apps like this.")
 
